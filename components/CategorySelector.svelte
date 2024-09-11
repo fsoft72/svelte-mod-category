dde9b25a841c96148d1f76ed9593b99a15505666
Fabio Rotondo <fabio.rotondo@gmail.com>
2024-09-11 10:52:57 +0200

    Using the storeCategory

--- components/CategorySelector.svelte
+++ components/CategorySelector.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
 	import SelectTree from '$liwe3/components/SelectTree.svelte';
-	import { type Tree, type TreeItem } from '$liwe3/utils/tree';
+	import { type Tree } from '$liwe3/utils/tree';
+	import { storeCategory } from '../store.svelte';
 
 	interface Props {
 		value?: string;
-		categories?: TreeItem[];
 
 		onchange: (value: string) => void;
 	}
 
-	let { value = '', categories = [], onchange }: Props = $props();
+	let { value = '', onchange }: Props = $props();
 </script>
 
-<SelectTree tree={categories} {value} {onchange} />
+<SelectTree tree={storeCategory.tree()} {value} {onchange} />
