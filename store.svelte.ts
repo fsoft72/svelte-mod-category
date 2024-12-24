c9247c27fa3ce21f4f4fd036a534cc6759e28e61
fsoft <fabio.rotondo@gmail.com>
2024-12-24 11:05:19 +0100

    ADD: enhance CategoryStore with adminLoad and fieldUpdate methods for improved category management

--- store.svelte.ts
+++ store.svelte.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { runeDebug } from '$liwe3/utils/runes.svelte';
-import { category_admin_add, category_admin_del, category_admin_update, category_list } from './actions';
-import type { Category, CategoryTreeItem } from './types';
+import { category_admin_add, category_admin_del, category_admin_list, category_admin_update, category_list } from './actions';
+import type { CategoryTreeItem } from './types';
 
 interface CategoryStore {
 	categories: CategoryTreeItem[];
@@ -9,14 +9,15 @@ interface CategoryStore {
 
 	get ( id: string ): CategoryTreeItem | undefined;
 	load ( force?: boolean ): Promise<any>;
+	adminLoad ( force?: boolean ): Promise<any>;
 	add ( item: CategoryTreeItem, skip_save?: boolean ): Promise<void>;
-	del ( item: Category, skip_save?: boolean ): Promise<void>;
-	update ( item: Category ): Promise<void>;
+	del ( item: CategoryTreeItem, skip_save?: boolean ): Promise<void>;
+	update ( item: CategoryTreeItem ): Promise<void>;
+	fieldUpdate ( id_item: string, field: string, value: any ): Promise<void>;
 
 	list (): { label: string, value: string; }[];
 	all (): CategoryTreeItem[];
-
-	_sort_parent ( item: CategoryTreeItem, skip_save?: boolean ): void;
+	getByParent ( id_parent?: string ): CategoryTreeItem[];
 }
 
 export const storeCategory: CategoryStore = $state( {
@@ -36,8 +37,6 @@ export const storeCategory: CategoryStore = $state( {
 		const res = await category_list();
 		if ( res.error ) return res;
 
-		console.log( "=== RES: ", res );
-
 		res.map( ( cat: CategoryTreeItem ) => {
 			storeCategory.categories.push( cat );
 			storeCategory.categoriesMap[ cat.id ] = cat;
@@ -52,23 +51,36 @@ export const storeCategory: CategoryStore = $state( {
 		return {};
 	},
 
-	_sort_parent ( item: CategoryTreeItem, skip_save = false ) {
-		const parent = storeCategory.categoriesMap[ item.id_parent ];
-		if ( !parent ) return;
+	async adminLoad ( force = false ) {
+		if ( !force && storeCategory.categories.length ) return {};
 
-		if ( !parent.children ) parent.children = [];
-		if ( !parent.children.includes( item ) ) parent.children.push( item );
-		if ( !skip_save ) storeCategory.update( parent as any );
+		storeCategory.categories.length = 0;
+		storeCategory.categoriesMap = {};
 
-		// Sort children by title
-		parent.children.sort( ( a, b ) => a.title.localeCompare( b.title ) );
+		const res = await category_admin_list();
+		if ( res.error ) return res;
 
-		// replace category in main list
-		const pos = storeCategory.categories.findIndex( ( cat ) => cat.id === parent.id );
-		if ( pos != -1 ) storeCategory.categories.splice( pos, 1 );
-		storeCategory.categories.splice( pos, 0, parent );
+		res.map( ( cat: CategoryTreeItem ) => {
+			if ( !cat.id_parent ) {
+				storeCategory.categories.push( cat );
+			}
 
-		storeCategory.categoriesMap[ item.id ] = item;
+			storeCategory.categoriesMap[ cat.id ] = cat;
+		} );
+
+		res.map( ( cat: CategoryTreeItem ) => {
+			if ( !cat.id_parent ) return;
+
+			const parent = storeCategory.categoriesMap[ cat.id_parent ];
+			if ( !parent ) return;
+
+			if ( !parent.children ) parent.children = [];
+			parent.is_folder = true;
+
+			parent.children.push( cat );
+		} );
+
+		return {};
 	},
 
 	async add ( item: CategoryTreeItem, skip_save = false ) {
@@ -77,29 +89,35 @@ export const storeCategory: CategoryStore = $state( {
 			if ( res.error ) return;
 		}
 
-		if ( item.id_parent ) {
-			storeCategory._sort_parent( item, true );
-		} else {
+		if ( !item.id_parent ) {
 			storeCategory.categories.push( item );
 
 			// Sort categories by title
 			storeCategory.categories.sort( ( a, b ) => a.title.localeCompare( b.title ) );
 		}
 		storeCategory.categoriesMap[ item.id ] = item;
-
-		console.log( "=== ADD: ", item );
 	},
 
 	async update ( item: CategoryTreeItem ) {
 		const res = await category_admin_update( item.id, item.id_parent, item.title, item.slug, item.description, item.modules, item.top, item.visible, item.image );
 		if ( res.error ) return;
 
+		/*
 		if ( item.id_parent ) {
 			storeCategory._sort_parent( item as any, true );
 		}
+			*/
 
-		storeCategory.del( item as any, true );
-		storeCategory.add( item as any, true );
+		// storeCategory.del( item as any, true );
+		// storeCategory.add( item as any, true );
+	},
+
+	async fieldUpdate ( id_item: string, field: string, value: any ) {
+		const item: Record<string, any> = storeCategory.categoriesMap[ id_item ];
+		if ( !item ) return;
+
+		item[ field ] = value;
+		await storeCategory.update( item as any );
 	},
 
 	async del ( item: CategoryTreeItem, skip_save = false ) {
@@ -121,5 +139,26 @@ export const storeCategory: CategoryStore = $state( {
 
 	all () {
 		return storeCategory.categories;
-	}
+	},
+
+	getByParent ( id_parent?: string ): CategoryTreeItem[] {
+		let items: CategoryTreeItem[] = [];
+
+		console.log( "=== getByParent: ", id_parent );
+		if ( !id_parent )
+			items = storeCategory.categories.filter( ( cat ) => !cat.id_parent );
+		else {
+			const parent = storeCategory.categoriesMap[ id_parent ];
+
+			if ( !parent || !parent.children ) return [];
+
+			items = parent.children;
+		}
+
+		// return items sorted by title without the category with '__empty__' slug
+		return items.filter(
+			( cat ) => cat.slug !== '__empty__'
+		).sort( ( a, b ) => a.title.localeCompare( b.title ) );
+
+	},
 } );
